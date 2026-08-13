1fbf5444ec22a6bf05f226126b438a41dae14f84
Karen Rustad Tölva <karen.rustad@gmail.com>
2026-08-13 02:13:45 -0700

    Make dist build with relative URLs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
+  base: "./",
   worker: {
-    format: 'es',
+    format: "es",
   },
-})
+});
